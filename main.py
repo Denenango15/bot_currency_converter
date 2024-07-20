ecbadd2ae84e349e8af0266dfb4926aececbe115
Denis <denisnesterov15@gmail.com>
2024-07-20 13:22:20 +0400

    requirements.txt

--- main.py
+++ main.py
@@ -9,7 +9,6 @@ import os
 from aiogram import Bot, Dispatcher
 from aiogram.filters import CommandStart, Command
 from aiogram.types import Message
-from config import redis_port, redis_host
 from dotenv import load_dotenv
 
 
